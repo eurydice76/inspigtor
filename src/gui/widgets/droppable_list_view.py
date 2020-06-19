a24a5c8fc4433061ea6dcea0906d248db7751444
Eric Pellegrini <ericpellegrini76@gmail.com>
2020-06-19 14:17:40 +0200

    change parameters name

--- src/gui/widgets/droppable_list_view.py
+++ src/gui/widgets/droppable_list_view.py
@@ -24,13 +24,13 @@ class DroppableListView(QtWidgets.QListView):
         else:
             e.ignore()
 
-    def dropEvent(self, e):
+    def dropEvent(self, event):
         """Event triggered when the dragged item is dropped into this widget.
         """
 
         # Copy the mime data into a source model to get their underlying value
         source_model = QtGui.QStandardItemModel()
-        source_model.dropMimeData(e.mimeData(), QtCore.Qt.CopyAction, 0, 0, QtCore.QModelIndex())
+        source_model.dropMimeData(event.mimeData(), QtCore.Qt.CopyAction, 0, 0, QtCore.QModelIndex())
 
         # Drop only those items which are not present in this widget
         current_items = [self.model().item(i, 0).text() for i in range(self.model().rowCount())]
